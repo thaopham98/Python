daefd213f383e3c0c82954b4cd204f84e18b4513
thaopham98 <79811818+thaopham98@users.noreply.github.com>
2023-05-01 03:23:16 -0700

    Creating a new folder to move the files.

--- LeetCode/sortedSquares.py
+++ LeetCode/sortedSquares.py
@@ -6,7 +6,6 @@ class Solution:
     #             for j in range(0, n-i-1):
     #                 if list[j] > list[j+1]:
     #                     list[j], list[j+1] = list[j+1], list[j]
-
     #         return list        
 
 
