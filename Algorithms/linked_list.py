2c1ed44c988041b2d25bbbd86483c3a047a52987
thaopham98 <79811818+thaopham98@users.noreply.github.com>
2023-05-03 03:25:26 -0700

    Adding append method to the LinkedList class.

--- Algorithms/linked_list.py
+++ Algorithms/linked_list.py
@@ -1,13 +1,54 @@
-#creating a node
+'''The class represents a single node in the linked list'''
 class Node:
     def __init__(self, data = None):
         self.data = data #the value of a node
         self.next = None #the node it's pointing to.
 
+class Linked:
+    def __init__(self, test):
+        self.test1 = test
+
+'''The Class represents the entire list '''
 class LinkedList:
     def __init__(self):
         self.head = None # Head node of the list
-    
+
+    '''Adding a new node to the end of the list'''
+    def append(self,data):
+        new_node = Node(data) # Creating a new node
+
+        '''If the list is empty, creating a new node and set as the head node'''
+        if not self.head:
+            self.head = new_node
+            return
+        
+        '''If the list is NOT empty, travesing to the end of the list and creating a new node'''
+        current_node = self.head
+        print(f'The list is not empty, creating a new node, {new_node.data}')
+
+        print(f'Starting the traversing throught the list ...')
+        while current_node.next: # As long as current_node is pointing to the next node,
+            current_node = current_node.next # Setting current_node as the next node.
+            
+            '''Displaying the traversing through the list'''
+            # if current_node.next:
+            #     print(f'{current_node.data} -> {current_node.next.data}')
+            # else:
+            #     print(f'{current_node.data} -> None')
+
+        '''
+        When the last node is reached, the `next` property of the current_node
+        is set to the `next_node` that is being added to the end of the list.
+        This effectively appends the new_node to the end of the linked list. 
+        '''
+        current_node.next = new_node 
+        print(f'The new node, {new_node.data} is added to the list')
+        # After the while loop completes, current_node will be pointing to the last node in the list
+        # So, we can append the new node to the end of the list
+
+
+
+    '''Displaying the contents of the list'''
     def printing(self):
         displaying = self.head #displaying the head node/ the starting node
         while displaying is not None: 
@@ -20,9 +61,13 @@ list1.head = Node("head")
 node1 = Node('1')
 node2 = Node('2')
 
-#Link head to node1
+'''Linking head to node1'''
 list1.head.next = node1
-#Link node1 to node2
+'''Linking node1 to node2'''
 node1.next = node2
 
+list1.printing()
+
+'''Appeding a new node to the list'''
+list1.append('3')
 list1.printing()
