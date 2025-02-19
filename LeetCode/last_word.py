8f1a8e6f2454234fa8023388e4b378d214c5ff7b
thaopham98 <minhppham14@gmail.com>
2025-02-18 17:40:45 -0800

    Update and Solve the Question

--- LeetCode/last_word.py
+++ LeetCode/last_word.py
@@ -1,14 +1,30 @@
-def lengthOfLastWord( s: str) -> int:
-        space = ' '
-        count = 0
-        for n in reversed(s):
-            if (n != space or s[-1] == ' '):
-                # print(n)
-                count += 1
-            # else:
-            #     return count
-            return count
-            
+"""
+Given a string `s` consisting of words and spaces, return the length of the last word in the string.
+"""
 
-length = lengthOfLastWord('Hello World ')
-print(length)
+def lengthOfLastWord( s: str) -> int:
+    
+    """
+    Handling empty OR all-space string
+    """
+    if len(s) == 0 or s.isspace(): # Check for all spaces OR empty string
+        return 0 
+
+    found_last_word = False
+    count = 0
+
+    """
+    since this is about the last word of a string, we can just start at the end of the string
+    using `reversed()`
+    """
+
+    for n in reversed(s):
+        if n != ' ': # skipping/ingoring the ' ' when counting the string 
+            found_last_word = True # signaling the it finds non-space char
+            count += 1 # start counting the non-space char
+        elif found_last_word: # # If you've already found the last word AND hit a space
+            break # the loop break/stop
+        
+    return count            
+            
+print(lengthOfLastWord(' fad sgfdf s '))
